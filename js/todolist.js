4f3a0cfb0687154a2bdec63d451753b38c12b5ea
Sangmin Park <sangminpark@Sangminui-MacBookPro.local>
2023-03-30 14:35:39 +0900

    first_todo_list

--- js/todolist.js
+++ js/todolist.js
@@ -1,56 +1,142 @@
-const toDoForm = document.getElementById("todo-form");
-const toDoInput = document.querySelector("#todo-form input");
-const toDoList = document.getElementById("todo-list");
-
-/* const toDoForm = document.getElementById("todo-form");
-const toDoInput = document.querySelector("#todo-form input");
-const toDoList = document.getElementById("todo-list"); */
+const todoInput = document.querySelector('.todo-input');
+const todoList = document.querySelector('.todo-list');
 
 const TODOS_KEY = "todos";
 
-let toDos = [];
+let todos = [];
+let id = 0;
 
-function saveToDos() {
-	localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+const setTodos = (newTodos) => {
+	todos = newTodos;
 }
 
-function deleteToDo(event) {
-	const li = event.target.parentElement;
-	li.remove(); 
+const getAllTodos = () => {
+	return todos;
 }
 
-function paintToDo(newTodo) {
-	const li = document.createElement("li");
-	//li.id = newTodo.id;
-	const span = document.createElement("span");
-	//span.innerText = newTodo.text;
-	span.innerText = newTodo;
-	const button = document.createElement("button");
-	button.innerText = "❌";
-	button.addEventListener("click", deleteToDo)
-	li.appendChild(span);
-	li.appendChild(button);
-	toDoList.appendChild(li);
-}
-
-function handleToDoSubmit(event) {
-	event.preventDefault();
-	const newTodo = toDoInput.value;
-	toDoInput.value = "";
-	toDos.push(newTodo);
-	paintToDo(newTodo);
+//concat 문자열을 합치는 함수 배열을 계속해서 붙쳐준다.
+const appendTodos = (text) => {
+	const newId = Date.now();
+	const newTodos = getAllTodos().concat({id: newId, isCompleted: false, content: text});
+	setTodos(newTodos);
+	paintTodos();
 	saveToDos();
 }
 
-toDoForm.addEventListener("submit", handleToDoSubmit);
+//map 주어진 함수를 조건에 맞게 변경후 반환
+const completeTodo = (todoId) => {
+	const newTodos = getAllTodos().map(todo => todo.id === todoId ? {...todo, isCompleted: !todo.isCompleted} : todo);
+	setTodos(newTodos);
+	paintTodos();
+	saveToDos();
+	console.log(newTodos);
+}
 
-function sayHello(item) {
-	console.log(item);
+const deleteToDo = (todoId) => {
+	const newTodos = getAllTodos().filter(todo => todo.id !== todoId);
+	setTodos(newTodos);
+	paintTodos();
+	saveToDos();
+}
+
+const updateTodo = (text, todoId) => {
+	const newTodos = getAllTodos().map(todo => todo.id === todoId ? ({...todo, content: text}) : todo);
+	setTodos(newTodos);
+	paintTodos();
+	saveToDos();
+}
+
+const onDbclickTodo = (e, todoId) => {
+	const todoElem = e.target;
+	const inputText = e.target.innerText;
+	const todoItem = todoElem.parentNode;
+	const inputElem = document.createElement('input');
+	inputElem.value = inputText;
+	inputElem.classList.add('edit-input');
+
+	inputElem.addEventListener('keypress', (e) => {
+		if(e.key === 'Enter' && e.target.value !== '') {
+			updateTodo(e.target.value, todoId);
+			document.body.removeEventListener('click', onClickBody);
+		}
+	})
+	//body를 클릭했을때 탈출
+	const onClickBody = (e) => {
+		if (e.target !== inputElem) {
+			todoItem.removeChild(inputElem);
+			document.body.removeEventListener('click', onClickBody);
+		}
+	}
+	document.body.addEventListener('click', onClickBody);
+	todoItem.appendChild(inputElem);
+}
+
+const saveToDos = () => {
+	localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
+
+const EachTodo = () => {
+	todoList.innerHTML = '';
+	const allTodos = getAllTodos();
+
+	allTodos.forEach(paintTodos(todo));
+}
+
+const paintTodos = () => {
+	todoList.innerHTML = '';
+	const allTodos = getAllTodos();
+
+	allTodos.forEach(todo => {
+		const todoItem = document.createElement('li');
+		todoItem.classList.add('todo-item');
+		
+		todoItem.setAttribute('data-id', todo.id);
+
+		const checkboxElem = document.createElement('div');
+		checkboxElem.classList.add('checkbox');
+		checkboxElem.addEventListener('click', () => completeTodo(todo.id));
+
+		const todoElem = document.createElement('div');
+		todoElem.classList.add('todo');
+		todoElem.addEventListener('dblclick', (event) => onDbclickTodo(event, todo.id));
+		todoElem.innerText = todo.content;
+
+		const delBtn = document.createElement('button');
+		delBtn.classList.add('delBtn');
+		delBtn.addEventListener('click', () => deleteToDo(todo.id));
+		delBtn.innerHTML = 'X';
+
+		if (todo.isCompleted) {
+			todoItem.classList.add('checked');
+			checkboxElem.innerText = '✔';
+		}
+
+		todoItem.appendChild(checkboxElem);
+		todoItem.appendChild(todoElem);
+		todoItem.appendChild(delBtn);
+
+		todoList.appendChild(todoItem);
+	})
+}
+
+//input요소로부터 이벤트리스너를 등록하여, 캐치한후, 입력받은 데이터를 배열에 순차적으로 담아준다.
+const init = () => {
+	todoInput.addEventListener('keypress', (e) =>{
+		if (e.key === 'Enter' && e.target.value !== ''){
+			console.log(e.target.value);
+			appendTodos(e.target.value); 
+			todoInput.value = '';
+		}
+	})
 }
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
-if(saveToDos !== null) {
+if(savedToDos !== null) {
 	const parsedToDos = JSON.parse(savedToDos);
-	parsedToDos.forEach(paintToDo);
+	console.log(parsedToDos);
+	todos = parsedToDos;
+	parsedToDos.forEach(paintTodos);
 }
+
+init();
